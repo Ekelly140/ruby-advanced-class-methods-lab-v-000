7f2c756ef7f6dff77768e7f8776d3c162351de00
ERIC KELLY <eakelly220@gmail.com>
2018-04-24 18:57:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,10 +16,10 @@ class Song
     song
   end 
   
-  def self.new_by_name(name)
+  def self.new_by_name(title)
     song = self.new 
     @@all << self.new 
-    song.name = name
+    song.name = title
   end 
 
 
